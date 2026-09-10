8bce656f4c849f1e1daa5206154828ea02b55bc7
Sebastian Pulido <sebastianpulido12p@gmail.com>
2026-09-10 13:23:13 -0500

    Actualizamos la tabla producto

--- app/models/producto.php
+++ app/models/producto.php
@@ -2,18 +2,30 @@
 
 require_once __DIR__ . "/../../config/Database.php";
 
-class Producto {
+class Producto
+{
     private $connection;
 
-    public function __construct(){
+    public function __construct()
+    {
         $db = new Database();
         $this->connection = $db->connect();
     }
 
-    public function getAll(){
-        $sql = "SELECT * FROM producto";
+    public function getAll()
+    {
+        $sql = "SELECT
+                    producto.id_producto,
+                    producto.nombre AS producto_nombre,
+                    producto.precio,
+                    producto.categoria AS producto_categoria,
+                    proveedores.nombre AS proveedor_nombre
+                FROM producto
+                INNER JOIN proveedores
+                    ON producto.id_proveedor = proveedores.idproveedores";
 
         $consulta = $this->connection->query($sql);
+
         return $consulta->fetchAll(PDO::FETCH_ASSOC);
     }
 }
--- app/views/producto/index.php
+++ app/views/producto/index.php
@@ -1,21 +1,20 @@
-<h1>Listado Producto</h1>
+<h1> Listado Producto </h1>
 
 <table border="1">
-
     <tr>
         <th>Nombre</th>
         <th>Precio</th>
-        <th>Categoria</th>
+        <th>Categoria Producto</th>
+        <th>Proveedor</th>
     </tr>
-
     <?php foreach ($productos as $producto): ?>
+            <tr>
+                <td><?= $producto['producto_nombre'] ?></td>
+                <td><?= $producto['precio'] ?></td>
+                <td><?= $producto['producto_categoria'] ?></td>
+                <td><?= $producto['proveedor_nombre'] ?></td>
+            </tr>
+        <?php endforeach; ?>
 
-        <tr>
-            <td><?= $producto['nombre'] ?></td>
-            <td><?= $producto['precio'] ?></td>
-            <td><?= $producto['categoria'] ?></td>
-        </tr>
-
-    <?php endforeach; ?>
 
 </table>
